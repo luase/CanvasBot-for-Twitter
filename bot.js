c8368bb72d8b8fb0caca4d105a8578adbabb1a7c
Jimmy Alonso <jasajsas@gmail.com>
2017-08-05 22:26:01 -0500

    Release!
    Forgot to actually implement the Twitter API last commit.

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ console.log('Initiating CanvasBot');
 var Twit = require('twit');
 var exec = require('child_process').exec;
 var config = require('./config');
+var fs = require('fs');
 var T = new Twit(config);
 var pxlcmd = 'processing-java --sketch=`pwd`/Canvas --run';
 
@@ -11,13 +12,38 @@ var stream = T.stream('user');
 stream.on('tweet', AddPixel);
 
 function AddPixel(info){
-    var PixelData = info.text.split(" ", 4);
-    var pycmd = 'python canvas/cc.py ' + PixelData[1] + ' ' + PixelData[2] + ' ' + PixelData[3];
-    console.log(pycmd);
-    exec(pycmd, processing);
-    function processing(){
-        exec(pxlcmd);
+
+    var PixelData = info.text.split(" ");
+    if (PixelData[0] == '@LienzoBot' && PixelData.length == 4){
+        var pycmd = 'python canvas/cc.py ' + PixelData[1] + ' ' + PixelData[2] + ' ' + PixelData[3];
+        exec(pycmd, processing);
+        function processing(){
+            exec(pxlcmd, imgcre);
+            function imgcre (){
+                console.log('Image Created!');
+
+        var FileName = 'Canvas/image.png'
+        var b64 = fs.readFileSync(FileName, { encoding: 'base64' });
+
+        T.post('media/upload', {media_data: b64}, uploaded);
+
+        function uploaded(err, data, response){
+            var id = data.media_id_string;
+            var tweet = {
+                status: 'Pixel (' + PixelData[1] + ', ' + PixelData[2] + ') edited by @' + info.user.screen_name,
+                media_ids: [id]
+            }
+            T.post('statuses/update', tweet, tweeted);
+        }
+
+        function tweeted(err, data, response){
+            if (err){
+                console.log("Something went wrong!");
+            } else {
+                console.log("It worked!");
+            }
+        }
+            }
+        }
     }
-    //console.log(info.text);
-    //console.log(info.user.screen_name);
 }
